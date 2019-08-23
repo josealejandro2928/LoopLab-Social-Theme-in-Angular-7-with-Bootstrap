91aee0cb70ba7694447dbc0fb6ac84f7afa6efdd
Jose Alejandro Concepcion <jalejandro2928@gmail.com>
2019-08-22 21:42:40 -0400

    terminado el proyecto

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -6,9 +6,6 @@ import { LoopLabComponent } from './loop-lab/loop-lab.component';
 import { Page1Component } from './page1/page1.component';
 import { MizuxeBookComponent } from './mizuxe-book/mizuxe-book.component';
 
-
-
-
 @NgModule({
   declarations: [
     LoopLabComponent,
--- src/app/frontend/mizuxe-book/mizuxe-book.component.ts
+++ src/app/frontend/mizuxe-book/mizuxe-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mizuxe-book',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MizuxeBookComponent implements OnInit {
 
-  constructor() { }
+  isNavbarCollapsed = true;
+  arrayUsers: any[] = [
+    {
+      name: 'Susan Martin',
+      image: '../../../assets/users/user2.jpg',
+      ocupation: 'Lead Writer',
+      description: 'Thus, our report can be better geared towards the monitoring and evaluation of its commitments.',
+    },
+    {
+      name: 'Jessic Loaren',
+      image: '../../../assets/users/user3.jpg',
+      ocupation: 'Photographer',
+      description: 'Thus, our report can be better geared towards the monitoring and evaluation of its commitments.',
+    },
+    {
+      name: 'Martin Yelish',
+      image: '../../../assets/users/user4.jpg',
+      ocupation: 'Builder',
+      description: 'Thus, our report can be better geared towards the monitoring and evaluation of its commitments.',
+    },
+    {
+      name: 'Tom Kat',
+      image: '../../../assets/users/user5.jpg',
+      ocupation: 'Baseball Player',
+      description: 'Thus, our report can be better geared towards the monitoring and evaluation of its commitments.',
+    }
+
+  ];
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  }
+
+  getSafeImage(url: string) {
+    return this.sanitizer.bypassSecurityTrustStyle(`url(${url})`);
   }
 
 }
